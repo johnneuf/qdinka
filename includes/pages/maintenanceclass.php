562944fde1a40ca409d1bc42d897ddd3183ad7a5
kendall <kendalldarkmere@gmail.com>
2015-04-13 11:52:16 -0700

    Finishing up the maintenance page

--- includes/pages/maintenanceclass.php
+++ includes/pages/maintenanceclass.php
@@ -13,12 +13,25 @@ class Maintenance extends template\TemplateBase {
     function __construct($title, $noTemplate = true, $maintenance = true)
     {
         parent::__construct($title, $noTemplate, $maintenance);
+
+        //Add CSS
+        $this->add_css('/stylesheets/maintenance.css');
     }
 
     protected function body()
     {
         ?>
-
+        <div class="bubble-wrapper">
+            <h1>Maintenance</h1>
+            <div class="bubble-content">
+                <div class="bubble-image-wrapper">
+                    <img src="/images/QdinkaFull25.png" title="Qdinka" alt="Qdinka Logo"/>
+                </div>
+                <div class="bubble-wording-wrapper">
+                    We are sorry but Qdinka is currently undergoing maintenance right now and we will be back up as soon as we can. We thank you for your patience.
+                </div>
+            </div>
+        </div>
     <?php
     }
 
