efe63c4fd6971e29aa5d04f8cb6f802947627f1f
Kendall Holm <kendalldarkmere@gmail.com>
2015-07-03 16:41:59 +0000

    Roberto Changes Friday - Kendall

--- includes/pages/CartPageClass.php
+++ includes/pages/CartPageClass.php
@@ -20,50 +20,98 @@ class CartPage extends TemplateBase {
 
         //CSS
         $this->add_css('/stylesheets/layout.css');
+		  $this->add_css('/stylesheets/CartLayouts.css');
     }
 
     protected function body()
     {
         ?>
 			
-			<div class="cart-page-section-wrapper">
+<div class="cart-page-section-wrapper">
                 <h2><?php echo "Shopping Cart"; ?></h2>
            
-         <div class="cart-card-wrapper">
-                    <div class="cart-card-content-wrapper">
-                        <div class="card-image">
-                            <a href=""><img src="/images/no-image.png" alt="No Image" /></a>
-                        </div>
-                        	<div class="card-info">
-                            <span class="card-title"><a href="">Product Name</a></span>
-                            <span class="card-price">$0.00</span>
-                       	 	</div>
-                    </div>
-                </div>
-         <div class="cart-card-wrapper">
-                    <div class="cart-card-content-wrapper">
-                        <div class="card-image">
-                            <a href=""><img src="/images/no-image.png" alt="No Image" /></a>
-                        </div>
-                        	<div class="card-info">
-                            <span class="card-title"><a href="">Product Name</a></span>
-                            <span class="card-price">$0.00</span>
-                        	</div>
-                    </div>
-                </div>
 			<div class="cart-card-wrapper">
                     <div class="cart-card-content-wrapper">
+                      	<div class="save">
+                        		Save for later
+                         <span class = "remove"> Remove Item </span>
+                        </div>
                         <div class="card-image">
                             <a href=""><img src="/images/no-image.png" alt="No Image" /></a>
+                            <div class="ProductName">
+                              Product Name
+                            </div>
+                            <div class="QuantityColorSize">Quantity: <br/>Color: <br/>Size: <br/></div>  
                         </div>
                         	<div class="card-info">
-                            <span class="card-title"><a href="">Product Name</a></span>
+                            <span class="card-title"><a href="">Contact Seller</a></span>
                             <span class="card-price">$0.00</span>
-                        	</div>
+                        </div>
                     </div>
-                </div>
-				
+         </div>	
+                            
+			<div class="cart-card-wrapper">
+                    <div class="cart-card-content-wrapper">
+                      	<div class="save">
+                        		Save for later
+                         <span class = "remove"> Remove Item </span>
+                        </div>
+                        <div class="card-image">
+                            <a href=""><img src="/images/no-image.png" alt="No Image" /></a>
+                            <div class="ProductName">
+                              Product Name
+                            </div>
+                            <div class="QuantityColorSize">Quantity: <br/>Color: <br/>Size: <br/></div>  
+                        </div>
+                        	<div class="card-info">
+                            <span class="card-title"><a href="">Contact Seller</a></span>
+                            <span class="card-price">$0.00</span>
+                        </div>
+                    </div>
+         </div>
+                            
+			<div class="cart-card-wrapper">
+                    <div class="cart-card-content-wrapper">
+                      	<div class="save">
+                        		Save for later
+                         <span class = "remove"> Remove Item </span>
+                        </div>
+                        <div class="card-image">
+                            <a href=""><img src="/images/no-image.png" alt="No Image" /></a>
+                            <div class="ProductName">
+                              Product Name
+                            </div>
+                            <div class="QuantityColorSize">Quantity: <br/>Color: <br/>Size: <br/></div>  
+                        </div>
+                        	<div class="card-info">
+                            <span class="card-title"><a href="">Contact Seller</a></span>
+                            <span class="card-price">$0.00</span>
+                        </div>
+                    </div>
+         </div>
+                    
+			<div class="cart-card-wrapper">
+                    <div class="cart-card-content-wrapper">
+                    	<div class ="cart-card-smallbox">
+        					 Subtotal
+                      <span class="boxprice">$0.00</span>
+                		</div>
+                    </div>
+         </div>
+                      
+			<div class="cart-card-wrapper">
+                    <div class="cart-card-content-wrapper">
+                        <div class="cart-card-smallbox">
+                          <a href="#">Continue to Checkout</a>
+                        </div>
+                    </div>
+         </div>
+                      
+                      
 </div>
+
+
+
 <?php
 }
 
