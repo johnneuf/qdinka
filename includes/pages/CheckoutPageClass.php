7457e556e565817a8ba6f85611a04b9fe3657cba
Kendall Holm <kendalldarkmere@gmail.com>
2015-08-08 14:16:52 +0000

    Last Changes from Roberto

--- includes/pages/CheckoutPageClass.php
+++ includes/pages/CheckoutPageClass.php
@@ -27,34 +27,25 @@ class CheckoutPage extends TemplateBase {
         ?>
 			
 <div class="checkout-page-section-wrapper">
-           
-			<div class="checkout-card-wrapper">
-                    <div class="checkout-card-content-wrapper">
-                      	<div class="ShoppingCart">
-                        		Shopping Cart
-                        </div>
-                        <div class="MiddleDiv">
-                            <form action= "" method="get">
-  										First name* <input type="text" name="fname"><br>
-  										Last name* <input type="text" name="lname"><br>
-                              Address* <input type="text" name="fname"><br>
-                              Address (contd.) <input type="text" name="fname"><br>
-                              City* <input type="text" name="fname"><br>
-                              State* <input type="text" name="fname"><br>
-                              Zip* <input type="text" name="fname"><br>
-                              Country* <input type="text" name="fname"><br>
-                              Enter gift message below <input type="text" name="fname"><br>
+  						
+        	<div class = "checkout-card-wrapper">
+                  <div class="OrderDetails">
+                      Order details
+                  </div>
+             </div>  			
                               
-  										<input type="submit" value="Continue">
-									 </form>                        
-                        </div>
-                        	<div class="card-info">
-                            <span class="card-title"><a href="">Contact Seller</a></span>
-                            <span class="card-price">$0.00</span>
-                        </div>
-                    </div>
-         </div>	
-                            
+         <div class = "checkout-card-wrapper">      
+  						<div class="ExpressCheckout">
+                      Express Checkout
+                  </div>
+         </div>                     
+  			<div class = "checkout-card-wrapper">
+  						<div class="ShoppingCart">
+                      Shopping Cart
+                  </div>
+         </div>
+                      
+                              
 			<div class="checkout-card-wrapper">
                     <div class="checkout-card-content-wrapper">
                       	<div class="save">
@@ -94,8 +85,46 @@ class CheckoutPage extends TemplateBase {
                     </div>
          </div>	                    
 			
+                            
+			<div class="checkout-card-wrapper"> 
+           			<div class="checkout-card-content-wrapper">
+                        <div class="MiddleDiv">
+                          		<div class ="TopOfDiv">
+                        			STEP 1. <span style = "color: #1C75BD"> Shipping Details </span>
+                       			 </div><br> 
+                            <form action= "" method="get">
+                             		<div style = "width: 96.5%">
+                              	<div style = "width: 45%; float: left">
+        										First Name*
+        										<input  name="first_name" type="text" />
+        									</div>
+                                 <div style = "width: 45%; float: right">   
+                                    Last Name*
+        										<input  name="last_name" type="text" />
+											</div>
+											</div>
+                              Address* <br> <input type="text" name="address"><br>
+                              Address (contd.) <br> <input type="text" name="addcont"><br>
+                              City* <br> <input type="text" name="city"><br>
+                              State* <br> <input type="text" name="state"><br>
+                              Zip* <br> <input type="text" name="zip"><br>
+                              Country* <br> <input type="text" name="country"><br>
+                              Enter gift message below <br>
+                              <input type = "text" placeholder = " Recipient" name = "recipient"/>
+  										<input type = "text" placeholder = " From" name = "from"/>
+                                
+                              <textarea name="Enter" placeholder = "Enter message here..."></textarea> 
+                                <br>
+                              <input type="submit" value="Continue">
+                             
+									 </form>                        
+                        </div>
+                        	
+                    </div>
+         </div>	                            
+                            
+                            
 </div>
-
 
 
 <?php
