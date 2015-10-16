37500011f4bf84cd4fba23cce0a15e8fbb424445
Partha Maiti <maiti.partha@yahoo.com>
2015-10-16 13:20:16 -0700

    rough draft

---
+++ schema/products.sql
@@ -0,0 +1,13 @@
+CREATE TABLE  products
+(
+product_id number (11) primary key,
+product_name varchar(25) NOT NULL,
+product_price number (11)NOT NULL,
+product_type varchar(25)NOT NULL,
+product_condition varchar(25) NOT NULL,
+product_shipping varchar(25) NOT NULL,
+product_rating varchar(25) NOT NULL,
+product_color varchar(25) NOT NULL,
+product_other varchar(25) NOT NULL
+
+);
