cc8b89603b9a59032bed920fba05aa49cfeb7bb6
Pradnya Bharambe <pradnya.bharambe@gmail.com>
2015-10-17 14:21:36 -0700

    MyAccountPage Commit

---
+++ pages/Myaccountpage/Myaccountpage.php
@@ -0,0 +1,86 @@
+
+<HTML>
+<head>
+<!--<link rel="import"" href="template.php"/>-->
+</head>
+<link rel="stylesheet" href="newcss.css" type="text/css"/>
+   <body>
+   
+   
+  <head>
+      <?php include 'mainheader.php';?>
+	  </head>
+    
+
+  <div class="buyer-top-Header">
+			
+           <h3> Sell unlimited products on the<u> 0% structure .</u> Post more, sell more, and make a higher percentage from each sale...</h3>
+	       
+		   <a href="#" class="upgrade-button" >Upgrade</a>
+		    
+		 
+		  
+  </div>
+  
+  <div class ="buyer-profile-wrapper">
+        <div class="buyer-image-wrapper">
+         <img src="https://github.com/johnneuf/qdinka/blob/master/images/icons/user.png?raw=true"/>
+          </div>
+   
+          <div class="buyer-profile-detail-wrapper">
+           <h2>My Account</h2>
+               <div class="buyername">
+			                     
+                  <span id="buyerAccountName"> Buyer Account Name</span> - <span id="buyerCompanyName">Buyer Comapany Name </span>
+				  <span><a href="#">edit</a></span>                   
+				  </div>
+                    <div>Reviews: <span id="Reviews">Number</span></div>
+                    <div>Sales: <span id="Sales">Number</span></div>
+                     
+            </div>
+            <div class="buyer-button-wrapper">
+
+                <span class="shop-button"><a href="#">Shop</a></span>
+                <span class="listing-button"><a href="#">Post Listing</a></span>
+                
+            </div>
+        </div>
+  </div>
+
+     
+       
+	<div class="welcome-area">
+	<p class="split-para">Welcome to My Account- <span>Go to your respective profile to manage either your purchases, or your sales.
+	Have fun experiencing the simplicity of the Qdinka marketplace!</span></p>
+		
+		<br/>
+		<hr>
+		<div class="buyer-profile-image">
+		<img src="https://github.com/johnneuf/qdinka/blob/master/images/icons/user.png?raw=true"/>
+		</div>
+		<div class="seller-profile-image">
+		<img src="https://github.com/johnneuf/qdinka/blob/master/images/icons/user.png?raw=true"/>
+		</div>
+    
+	 </div>
+	 
+	 <div class="listing-area">
+	   <h3 class="split-para"> Create Your Listing</h3>
+	 <p>Qdinka has created the best platform where young inventors and veteran entrepreneurs alike can come  to buy and sell 
+	 new technologies, risk free - we handle the processing</p>
+	 
+	 
+	 
+	 <a href="" class="welcome-area-listing-button">Post Listing</a>
+	 
+	 
+	 </div>
+ <div>        
+<?php include'mainfooter.php';?>
+</div>
+
+   
+  </body>
+  
+  
+<HTML>
