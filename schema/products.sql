fbd72a82d9d81b90a3862c29cab731cf5ccb610d
kendall <kendalldarkmere@gmail.com>
2015-10-19 20:39:21 -0700

    Changes to the Schema

--- schema/products.sql
+++ schema/products.sql
@@ -1,13 +1,14 @@
-CREATE TABLE  products
-(
-product_id number (11) primary key,
-product_name varchar(25) NOT NULL,
-product_price number (11)NOT NULL,
-product_type varchar(25)NOT NULL,
-product_condition varchar(25) NOT NULL,
-product_shipping varchar(25) NOT NULL,
-product_rating varchar(25) NOT NULL,
-product_color varchar(25) NOT NULL,
-product_other varchar(25) NOT NULL
-
-);
+CREATE TABLE `qdinka3`.`products` (
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `name` VARCHAR(45) NOT NULL COMMENT '',
+  `productType` INT NOT NULL COMMENT '',
+  `sellerID` INT NOT NULL COMMENT '',
+  `price` DECIMAL(6,2) NOT NULL DEFAULT 0 COMMENT '',
+  `inStock` BINARY(1) NOT NULL DEFAULT 1 COMMENT '',
+  `availible` INT(11) NOT NULL DEFAULT 0 COMMENT '',
+  `condition` INT(1) NOT NULL COMMENT '',
+  `description` TEXT NOT NULL COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '',
+  INDEX `seller` (`sellerID` ASC)  COMMENT '',
+  INDEX `type` (`productType` ASC)  COMMENT '')
+COMMENT = 'Table that stores all of the customers products';
