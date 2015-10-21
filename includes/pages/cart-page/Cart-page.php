71ae5ecd7c7e0f3a7190ac35125df7087783fa6b
Pradnya Bharambe <pradnya.bharambe@gmail.com>
2015-10-21 09:40:10 -0700

    Cart-page-commit

---
+++ includes/pages/cart-page/Cart-page.php
@@ -0,0 +1,122 @@
+<html>
+<head>
+      <?php include 'mainheader.php';?>
+	 </head>
+	 <link rel="stylesheet" href="Cart-page-css.css" type="text/css"/>
+	 <body>
+	 <div class="cart-details-wrapper">
+	                <div class="menu-wrapper">
+					<div class="box1"><a class="b" href="#">Shopping Cart</a></div>
+					<div class="box2"><a href="#">Express Checkout</a></div>
+					<div class="box3"><a href="#">Order Summary</a></div>
+			        </div>
+			 
+			
+			<div class="product-wrapper1">
+             
+                    <div class="save-for-later"><a href="#">save for later</a></div>
+                    <div class="remove-item"><a href="#">Remove Item</a></div>
+                    <div class="product-image"><img src="https://github.com/johnneuf/qdinka/blob/master/images/placeholder-product-image-large.png?raw=true" /></div>
+                    <div class="item-name"><a href="#">pink earbud wireless product</a></div>
+					<div class="product-specification">
+			        Quantity:<span id="quantity"></span><br>
+                    Color:<span id="color"></span><br>
+                    Size:<span id="size"></span>
+                     </div><br>
+                    <div class="vender"><a href="">contact seller</a></div>
+                    <div class="price-wrapper">$<span id="price">15.98</span></div>
+                     </div>
+					<div class="product-wrapper2">
+               
+                    <div class="save-for-later"><a href="#">save for later</a></div>
+                    <div class="remove-item"><a href="#">Remove Item</a></div>
+                    <div class="product-image"><img src="https://github.com/johnneuf/qdinka/blob/master/images/placeholder-product-image-large.png?raw=true" /></div>
+                    <div class="item-name"><a href="#">pink earbud wireless product</a></div>
+					<div class="product-specification">
+			        Quantity:<span id="quantity"></span><br>
+                    Color:<span id="color"></span><br>
+                    Size:<span id="size"></span>
+                     </div><br>
+					<div class="vender"><a href="">contact seller</a></div>
+                    <div class="price-wrapper">$<span id="price">15.98</span></div>
+                    </div>
+					<div class="product-wrapper3">
+                
+                    <div class="save-for-later"><a href="#">save for later</a></div>
+                    <div class="remove-item"><a href="#">Remove Item</a></div>
+                    <div class="product-image"><img src="https://github.com/johnneuf/qdinka/blob/master/images/placeholder-product-image-large.png?raw=true" /></div>
+                    <div class="item-name"><a href="#">pink earbud wireless product</a></div>
+					<div class="product-specification">
+			        Quantity:<span id="quantity"></span><br>
+                    Color:<span id="color"></span><br>
+                    Size:<span id="size"></span>
+                     </div><br>
+                    <div class="vender"><a href="">contact seller</a></div>
+                    <div class="price-wrapper">$<span id="price">15.98</span></div>
+                    </div>
+					<div>
+					<a href="" class="checkout-button">Continue To Checkout</a>
+					<a href="" class="subtotal-wrapper">SubTotal:$<span id="sub-total">80.00</span></a>
+					</div>
+			
+			
+					
+	
+ </div>
+		   
+	 <div class="create-listing-wrapper">
+	 <h3> Create your Listing</h3>
+	 <br>
+	 <p>Qdinka has created the best platform where young inventors and veteran entrepreneurs alike can come 
+	 to buy and sell new technologies, risk free - we handle the processing</p><br>
+	 <a href="" class="Post-listing-button">Post Listing</a>
+	 <a href="" class="listing-guide-button">Listing Guide</a>
+	 </div>
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 <!--footer-->
+	   <div class="footer"><?php include 'mainfooter.php';?></div>
+	   </body>
+	   
+	  </html>
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 </body>
+	 </html>
