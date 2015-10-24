6497645cd261089c684256ec0c3b8a8799b49c66
Pradnya Bharambe <pradnya.bharambe@gmail.com>
2015-10-24 14:33:58 -0700

    header commit

---
+++ includes/pages/Myaccount.php/mainheader.php
@@ -0,0 +1,24 @@
+
+          <link rel="stylesheet" type="text/css" href="base.css">
+		  
+            <div class="template-header-wrapper">
+            <div class="template-logo-wrapper">
+            <a href=""> <img src="/images/QdinkaFull25.png"></a>
+            </div>
+            <div class="template-search-bar-wrapper">
+            <form action="" method="post">
+            <input type="text" name="txtSearch" placeholder="Search  ..."/>
+            <input type="image" name="btnSubmit" src="/images/icons/search.png" />
+            </form>
+             </div>
+           <div class="template-header-menu">
+            <ul>
+                <li class="template-header-menu-right"><a href="/index.php"><img class="template-header-menu-right" src="/images/icons/Shopping-Cart.png" alt="shopping cart" title="Shopping Cart"/></a></li>
+                <li><a href="">|Login</a></li>
+				</ul>
+				
+				</div>
+                </div>
+	
+	
+    
