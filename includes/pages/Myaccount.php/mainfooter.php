e9857831f6910ca5ad3b4b666dddb48298839da2
Pradnya Bharambe <pradnya.bharambe@gmail.com>
2015-10-24 14:37:21 -0700

    footer commit

---
+++ includes/pages/Myaccount.php/mainfooter.php
@@ -0,0 +1,40 @@
+<link rel="stylesheet" type="text/css" href="base.css">
+    <footer>
+    <div class="template-footer-wrapper">
+
+    <div class="template-footer-menu-wrapper">
+
+	
+    <div class="menu-item"><a href="#">About Qdinka</a></div>
+    <div class="menu-item"><a href="#">Community</a></div>
+    <div class="menu-item"><a href="#">Seller Information Center</a></div>
+    <div class="menu-item"><a href="#">Policies</a></div>
+    <div class="menu-item"><a href="#">Affiliates and Partners</a></div>
+    <div class="menu-item"><a href="#">Help/Contact</a></div>
+    <br>
+<hr/>
+</div>
+<div class="template-footer-content-wrapper">
+<div class="footer-image-wrapper">
+<a href=""><img class="footer-image" src="/images/QdinkaFull25.png" /> </a>
+</div>
+<div class="footer-content">
+A Note from the founders:<br />
+Created in order to bring a balance to the Online Marketplace industry, we want our users to actually enjoy selling on
+the internet, and not have to worry about hundreds of hidden fees, unnavigable seller profiles, and unreliable customer
+service. Our users are guaranteed a more than satisfactory experience with the most comprehensive seller platform
+available; post products, track sales, integrate QuickBooks, market and promote your business and products.
+</div>
+<div class="footer-social-wrapper">
+<img class="social-image" src="/images/social_media/facebook-icon.png" />
+<img class="social-image" src="/images/social_media/twitter-icon.png" />
+<img class="social-image" src="/images/social_media/google-icon.png" />
+</div>
+</div>
+
+</div>
+</footer>
+    
+
+
+    
