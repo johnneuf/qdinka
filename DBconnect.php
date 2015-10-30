2dd50ed29fbaf5371e476a4dc932f38d0f2afc17
Partha Maiti <maiti.partha@yahoo.com>
2015-10-30 12:17:20 -0700

    class2staticconectiontodb

---
+++ DBconnect.php
@@ -0,0 +1,25 @@
+<?php
+class DBconnect{
+	//connection to be opened
+	private static $connection=False;
+	
+	//connection values
+	private static $database_server=NULL; 
+	private static $database_username=NULL;
+	private static $database_password=NULL;
+	private static $database_name=NULL;
+	
+	//stores connection values
+	public static function conn2db($db_server,$db_username,$db_password,$db_name){
+		
+	}
+	
+	//
+	public static function query($query_database)
+	
+	
+	
+}
+
+
+;?>
