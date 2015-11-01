d3ec65b3f01c4b57bde84ed5decf399ae5264f4e
Nannalin Higgins <nhigg2@uis.edu>
2015-10-31 17:44:38 -0700

    - Added Notifications Page - Added highlight on the current tab at the navigation menus of the Manage Account Details - Reduced code duplication by combining common elements

---
+++ includes/pages/buyers/BuyerManageAccountContentBodyHead.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+* Nannalin Higgins
+* Created on 9/16/2015 at 3:20 PM
+* @email nannalin14@gmail.com
+*/
+
+echo '
+
+<!--- Buyer Body area --->
+            <div class="buyer-content-menu-wrapper">
+                <h2>Manage Account Details - </h2>
+                <div class="buyer-content-menu-detail">
+                    View, edit, and update your account information. 
+                    Make all information sound and accurate so sellers may smoothly fufil your orders.                
+                </div>
+            </div>
+            
+            <div class="buyer-content-body">
+                <div class="subheader-wrapper">
+                    <ul class="subheader-nav">
+                        <li class="personalInfo"><a href="#">Personal Info</a></li>
+                        <li class="paymentInfo"><a href="#">Payment Info</a></li>
+                        <li class="notifications"><a href="#">Notifications</a></li>
+                        <li class="security"><a href="#">Security</a></li>
+                        <li class="sellerProfile"><a href="#">Seller Profile</a></li>
+                    </ul>
+                </div>  
+';
+?>
