9927db2816dddb7e74072c3ad005887460940841
Partha Maiti <maiti.partha@yahoo.com>
2015-11-06 13:26:01 -0800

    static connection to db blueprint

--- DBconnect.php
+++ DBconnect.php
@@ -4,18 +4,37 @@ class DBconnect{
 	private static $connection=False;
 	
 	//connection values
-	private static $database_server=NULL; 
-	private static $database_username=NULL;
-	private static $database_password=NULL;
-	private static $database_name=NULL;
+	private static $db_server=""; 
+	private static $db_username="";
+	private static $db_password="";
+	private static $db_name="";
 	
 	//stores connection values
-	public static function conn2db($db_server,$db_username,$db_password,$db_name){
+	private static function setupconn($db_server,$db_username,$db_password,$db_name){
 		
 	}
 	
-	//
-	public static function query($query_database)
+	//quires database
+	$query_database=get_user_inp();
+	public static function query($query_database){
+		
+		
+	}
+	//test connection and query database or return error
+	public static function conn2db(){
+		try{
+			$conn  = new PDO (DBconnect::setupconn);
+			$query = self::query;
+			//set PDO error mode to exception
+			$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+			echo"connection successful";
+		}catch(PDOException $e){
+			echo"connection failed". $e->getMessage();
+		}
+		
+		
+		
+	}
 	
 	
 	
