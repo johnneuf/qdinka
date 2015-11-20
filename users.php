e390aaf9cd71eca7e18126ef6eeb8339964f1ea5
Partha Maiti <maiti.partha@yahoo.com>
2015-11-20 12:34:43 -0800

    users page conversion to a class blueprint

--- users.php
+++ users.php
@@ -1,12 +1,14 @@
 <html>
 	<body>
 		<?php
+		
 		//defines variables and sets to empty values
 		$firstName=$lastName=$email=$userID=$userName=$password="";
 		$fnERR=$lnERR=$emailERR=$userIDERR=$userNameERR=$passwordERR="";
 		
 		//tests user input on the names of the form controls
-		function test_user_input($user_input){
+		function test_user_input($user_input)
+		{
 			$user_input=htmlspecialchars($user_input);
 			$user_input=trim($user_input);
 			$user_input=stripslashes($user_input);
@@ -14,52 +16,62 @@
 		}
 		
 		//When form data is sent with http POST method
-		if ($_SERVER["REQUEST_METHOD"]=="POST"{
-			if(empty($_POST["firstName"]){
+		if ($_SERVER["REQUEST_METHOD"]=="POST")
+		{
+			if(empty($_POST["firstName"])
+			{
 				$fnERR="first name required";
 			}else
 			{
 				$firstName=test_user_input($_POST["firstName"]);
-				if(!preg_match("/^[a-zA-Z ]*$/",$firstName)){
+				if(!preg_match("/^[a-zA-Z ]*$/",$firstName))
+				{
 					$fnERR="letter and white space allowed only";
 				}
-				}
-			if(empty($_POST["lastName"]){
+			}
+			if(empty($_POST["lastName"])
+			{
 				$lnERR="last name required";
 			}else
 			{
 				$lastName=test_user_input($_POST["lastName"]);
-				if(!preg_match("/^[a-zA-Z ]*$/",$lastName)){
+				if(!preg_match("/^[a-zA-Z ]*$/",$lastName))
+				{
 					$lnERR="only letters and white space are allowed";
 				}
-				}
-			if(empty($_POST["email"]){
+			}
+			if(empty($_POST["email"])
+			{
 				$emailERR="email required";
 			}else
 			{
 				$email=test_user_input($_POST["email"]);
-				if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+				if(!filter_var($email,FILTER_VALIDATE_EMAIL))
+				{
 					$emailERR="invalid email format";
 				}
-				}
-			if(empty($_POST["userID"])){
+			}
+			if(empty($_POST["userID"]))
+			{
 				$userIDERR="user id required";
 			}else
 			{
 				$userID=test_user_input($_POST["userID"]);
-				}
-			if(empty($_POST["userName"])){
+			}
+			if(empty($_POST["userName"]))
+			{
 				$userNameERR="user name required";
 			}else
 			{
 				$userName=test_user_input($_POST["userName"]);
-				}
-			if(empty($_POST["password"])){
+			}
+			if(empty($_POST["password"]))
+			{
 				$passwordERR="password required";
 			}else
 			{
 				$password=test_user_input($_POST["password"]);
-				}
+			}
 			
 			
 		}
