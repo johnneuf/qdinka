d0570149069b7a863f1e40df3d36e49792952393
Partha Maiti <maiti.partha@yahoo.com>
2015-12-18 15:51:01 -0800

    rough draft of control file

---
+++ control_file.php
@@ -0,0 +1,10 @@
+<?php
+/**
+*@author Partha Maiti <maiti.partha@yahoo.com>
+*this is a controlfile
+*this php.file serves to include or require all the other files necessary to make the the Qdinka website work
+*goal of this php file will be to use the least amount of code to while gluing together and making a dynamic Qdinka webpage
+*/
+
+
+;?>
