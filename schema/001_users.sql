ec6fcb32f676dff28c8196094f16d9156a383204
Partha Maiti <maiti.partha@yahoo.com>
2016-01-25 15:03:51 -0800

    rough drafts

---
+++ schema/001_users.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.11.1deb2+deb7u1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 13, 2015 at 08:34 AM
+-- Server version: 5.5.41
+-- PHP Version: 5.4.39-0+deb7u2
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `qdinka_secure`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT 'User Record ID number',
+  `userName` varchar(30) NOT NULL COMMENT 'User Name',
+  `emailAddress` varchar(30) NOT NULL COMMENT 'Email Address for the user',
+  `company` varchar(30) NOT NULL,
+  `password` varchar(128) NOT NULL COMMENT 'Password for the account',
+  `salt` varchar(128) NOT NULL COMMENT 'Salt for the password',
+  `privs` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`ID`, `userName`, `emailAddress`, `company`, `password`, `salt`, `privs`) VALUES
+(1, 'backdoor', 'backdoor@qdinka.com', '', 'a07083acd458db60b73fb9c54a27ad9dcf894cdddea74b1a8683279d6f0f94c6771417736dba549de3a4026b62ee464b5e4bb48c8c2a3d188f9e286a3f9eb286', 'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef', 26);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
