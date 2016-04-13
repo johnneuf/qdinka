6e437d80ddf76fe0c1ac292bee7ae35d58c2a15a
Pradnya Bharambe <pradnya.bharambe@gmail.com>
2016-04-13 11:40:26 -0700

    Adding client side validation

--- includes/pages/Loginpage/loginpageClass.php
+++ includes/pages/Loginpage/loginpageClass.php
@@ -4,6 +4,7 @@
 namespace includes\pages\Loginpage;
 
 use includes\template\TemplateBase;
+use includes\autoload;
 
 class loginpage extends TemplateBase {
 
@@ -12,7 +13,8 @@ class loginpage extends TemplateBase {
 
         //CSS
         $this->add_css('/stylesheets/login.css');
-
+		//js
+       $this->add_js('/javascript/angular.min.js');
     }
 protected function body() {
 ?>
@@ -28,7 +30,7 @@ protected function body() {
 	     </div>
 		 
          </div>
-		 <div class="container">
+		 <div ng-app="" class="container" >
             <div class="left-col">
 	           <div class ="products-wrapper">
 	<a href="">All Products</a> 
@@ -66,35 +68,60 @@ protected function body() {
 	
 
 	</div>
+	
+	<?php
+if(isset($_POST['submit'])){
+
+	
+	$FirstName=$_POST['FirstName'];
+	$LastName=$_POST['LastName'];
+	$Email=$_POST['Email'];
+	$Password=$_POST['Password'];
+	$Username=$_POST['Username'];
+	
+	
+	
+}
+	?>
+	
                   
 					 <div class ="register-wrapper">
 	   
-	<form class="form" method="POST" action="">
+	<form class="form" name="form" method="POST" action="LogIn.php">
 	<h1>Register</h1>
 	<hr/>
 	
 	<label>First name</label>
-         <input class="inputbox" type="text" name="first name">
-        
+     <input class="inputbox" type="text" name="FirstName" ng-model="FirstName" required>
+     <p ng-show="form.FirstName.$touched && form.FirstName.$invalid"> FirstName is required.</p>
         <label>Last name</label>
-        <input class="inputbox"  type="text" name="last name">
+        <input class="inputbox"  type="text" name="LastName" ng-model="LastName" required>
+		<p ng-show="form.LastName.$touched && form.LastName.$invalid"> LastName is required.</p>
 		<label> Email</label>
-        <input class="inputbox" type="email" name="email">
-       
+        <input class="inputbox" type="email" name="Email" ng-model="Email" required>
+       <p ng-show="form.Email.$touched && form.Email.$invalid">Invalid Email .</p>
          <label>Password</label>
-        <input class="inputbox"  type="password" name="password">
-		
+        <input class="inputbox"  type="password" name="Password" ng-model="Password" required>
+		<p ng-show="form.Password.$touched && form.Password.$invalid"> Invalid Password</p>
 		 <label>Confirm Password</label>
-        <input class="inputbox"  type="password" name="password">
+        <input class="inputbox"  type="password" name="Confirm Password">
 		
 		<label>Username</label>
-        <input class="inputbox" type="username" name="username"> 
+        <input class="inputbox" type="username" name="Username" ng-model="Username" ng-minlength="5" ng-maxlength="20" ng-pattern="/^[a-zA-Z0-9]{4,10}$/" required }"
+      autofocus> 
+		 <p  ng-show="form.Username.$dirty && form.Username.$invalid">Error:
+        
+
+		<p ng-if="form.Username.$touched && form.Username.$invalid || form.Username.$error.pattern">Username pattern is invalid</p>
+		<p ng-if="form.Username.$error.minlength">Must be more than 5 characters.</p>
+         <p ng-if="form.Username.$error.maxlength">Must be less than 20 characters.</p>
+		 
 		<br>
 		
 		 <div>
-        <label><input class="checkbox" type="checkbox" />I want to recieve emails on Qdinka news and recently added product. </label>
+        <label><input class="checkbox" type="checkbox" value="yes" name="RecieveEmails" />I want to recieve emails on Qdinka news and recently added product. </label>
         </div>
-	    <input class="button" type="submit" name="Register"><br>
+	    <input class="button" type="submit" name="submit" value="submit"><br>
 		By registering, you confirm that you accept our<u> Terms of Use </u>and <u>Privacy Policy.</u>
 	
 	</form>
@@ -108,7 +135,7 @@ protected function body() {
 		 <h1>Log-in</h1>
 		 <hr/>
 		 <label>Username<br></label>
-         <input class="inputbox" type=""text" name="username">
+         <input class="inputbox" type=""text" name="username" ng-model="username">
          <br>
          <label>Password<br></label>
          <input class="inputbox" type="text" name="last name">
