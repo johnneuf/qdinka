869f9b5a65a8a330aac84ddf552f117ac4022963
Partha Maiti <maiti.partha@yahoo.com>
2016-04-13 19:27:56 -0700

    changes

--- includes/pages/DB_Connect_Insert_User_Input.php
+++ includes/pages/DB_Connect_Insert_User_Input.php
@@ -6,38 +6,39 @@ class Dbase_connect_insert_user_input
 {
 	//variables needed in establishing a connection between php and database server using PDO class
 	public static $server_name = "localhost";
-	public static $dbname = "";
-	public static $username = "";
-	public static $password = "";
+	public static $dbname = "pman";
+	public static $username = "partha";
+	public static $password = "Number11";
+}
 	
 	try
 	{
 		//establish the connection using PDO class and create an instance of the PDO class and thus an object
-		$pdoobjconnec = new PDO("mysql:host =; dbname = ",$username,$password);
+		$pdoconnecobj = new PDO("mysql:host = Dbase_connect_insert_user_input::$server_name; dbname = Dbase_connect_insert_user_input::$dbname", $username = Dbase_connect_insert_user_input::$username ,$password = Dbase_connect_insert_user_input::$password);
 		
 		//set the errormode to throwing an exception
-		$pdoobjconnec -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+		$pdoconnecobj -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 		
 		//constructing the sql query
-		$sql_insert = "INSERT INTO () VALUES()";
+		$sql = "SELECT * FROM new_users";
 		
 		//make a prepared statements for repeated later execution with different values and the prepared statement is essentially a PDO statement object from the PDO statement class
-		$pdostmntobj = $connec -> prepare($sql_insert)
+		$pdostmntobj = $pdoconnecobj -> query($sql);
 		
-		//bind parameters to the prepared statements
-		$pdostmntobj -> bindParam();
 		
-		//inserting a row of values which corresponds with its given binded parameters
+		//fetch the result
+		$pdostmntobjresult = $pdostmntobj -> fetchAll();
 		
-		$pdostmntobj -> exec();
-		echo "row of values successfully inserted into db";
+		//display the result of the query
+		var_dump($pdostmntobjresult);
+		
 	//	catching errors with PDO 
-	} 
-	catch (PDOException $e)
+	} catch (PDOException $e)
 	{
 		echo "error".$e -> getMessage ();
+		die();
 	}
-	$connec = null;
-}
+	
+
 
 ;?>
