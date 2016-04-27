97f74a4f114e39c7511f96ed29b2f2a3bb4b195a
Partha Maiti <maiti.partha@yahoo.com>
2016-04-27 16:29:38 -0700

    zip code validation initialization

---
+++ includes/pages/CartPage/UserInputFieldsBuyerZipCodePageClass.php
@@ -0,0 +1,13 @@
+<?php
+	
+	//creating an algorithm to validate zip code
+	//using a data structure ; function
+	function ($user_entered_zip_code)
+	{
+		//using a control structure to first determine if the zip code user entered is proper length
+		
+		//add on the - and four digits the the zip code user entered
+		
+	}
+
+;?>
