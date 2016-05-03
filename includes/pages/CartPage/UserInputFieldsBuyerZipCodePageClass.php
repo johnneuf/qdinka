da5534351df7d4a5b87f001253087bae57157fbf
Partha Maiti <maiti.partha@yahoo.com>
2016-05-02 18:54:27 -0700

    changes

--- includes/pages/CartPage/UserInputFieldsBuyerZipCodePageClass.php
+++ includes/pages/CartPage/UserInputFieldsBuyerZipCodePageClass.php
@@ -5,8 +5,13 @@
 	function ($user_entered_zip_code)
 	{
 		//using a control structure to first determine if the zip code user entered is proper length
+		if (strlen($user_entered_zip_code) == 5  && preg_match("/^([0-9]{5})$/i",$user_entered_zip_code) )
+		{
+			//add on the respective - and four digits to the zip code user entered use web API tools if possible
+			$user_entered_zip_code = ;
+		}
 		
-		//add on the - and four digits the the zip code user entered
+		
 		
 	}
 
